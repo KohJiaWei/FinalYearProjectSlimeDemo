2f9ebdbf259eb77443f5fddb7e31194705d2ffb5
Koh Jia Wei <kohjiawei1@gmail.com>
2024-12-13 22:30:13 +0800

    card game can replay

--- FinalYearProject/Assets/CardGameMain.cs
+++ FinalYearProject/Assets/CardGameMain.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-
+using static CardGameMain;
 
 public class CardGameMain : MonoBehaviour
 {
     // Start is called before the first frame update
-        public enum cardTypes
+    public enum cardTypes
     {
         diamond = 1,
         spade = 2,
@@ -17,9 +16,9 @@ public class CardGameMain : MonoBehaviour
         joker = 5,
         orange_mushroom = 6,
         slime = 7,
-        yeti = 8
-        
+        yeti = 8,
     }
+
     public static CardGameMain main;
     public CardUI prefabCardUI;
     public RectTransform canvas;
@@ -32,62 +31,94 @@ public class CardGameMain : MonoBehaviour
     public Sprite slimeSprite;
     public Sprite yetiSprite;
 
-    
     CardUI card1;
     CardUI card2;
 
     int count = 0;
-    
+    int max_x = 4;
+    int max_y = 4;
+    bool isCurrentlyChecking = false;
+    List<GameObject> listOfCardsObject = new List<GameObject>();
 
-    public void onCardSelection(CardUI cardUIelement){
-        
-
-        if (card1 == null){
-            card1 = cardUIelement;
-            card1.PlayAnim("FlipOpen");
+    public void onCardSelection(CardUI cardUIelement)
+    {
+        if (isCurrentlyChecking)
+        {
             return;
         }
-        
-        if (card2 == null){
-            card2 = cardUIelement;
+        if (card1 == null)
+        {
+            card1 = cardUIelement;
+            card1.PlayAnim("FlipOpen", () => { });
+            return;
         }
-        card2.PlayAnim("FlipOpen");
-
-
-        if (card1.cardType == card2.cardType){
-            card1.matched = true;
-            card2.matched = true;
-            count += 1;
-            
+        if (card1 == cardUIelement)
+        {
+            return;
         }
-        else{
+        card2 = cardUIelement; //right hand doesnt matter whether have card or not
+        isCurrentlyChecking = true;
+        card2.PlayAnim(
+            "FlipOpen",
+            () =>
+            {
+                if (card1.cardType == card2.cardType)
+                {
+                    card1.isAlreadyMatchedWithAnotherCard = true;
+                    card2.isAlreadyMatchedWithAnotherCard = true;
+                    count += 1;
+                }
+                else
+                {
+                    card1.PlayAnim("FlipClose", () => { });
+                    card2.PlayAnim("FlipClose", () => { });
+                }
+                card1 = null;
+                card2 = null;
+                isCurrentlyChecking = false;
+            }
+        );
 
-            card1.PlayAnim("FlipClose");
-            card2.PlayAnim("FlipClose");
-        }
-        card1 = null;
-        card2 = null;
         return;
     }
 
-    void Start()
+    //
+
+    void StartGame()
     {
-        main = this;
-        var x_pos = -350;//-700 : 700
+        var x_pos = -350; //-700 : 700
         var y_pos = 400; //-400 : 400
-        cardTypes[] cardsType = new cardTypes[]{ cardTypes.club, cardTypes.spade, cardTypes.heart, cardTypes.diamond,
-            cardTypes.joker, cardTypes.orange_mushroom, cardTypes.slime, cardTypes.yeti,
-            cardTypes.club, cardTypes.spade, cardTypes.heart, cardTypes.diamond,
-            cardTypes.joker, cardTypes.orange_mushroom, cardTypes.slime, cardTypes.yeti};
+        
+        cardTypes[] cardsType = new cardTypes[]
+        {
+            cardTypes.club,
+            cardTypes.spade,
+            cardTypes.heart,
+            cardTypes.diamond,
+            cardTypes.joker,
+            cardTypes.orange_mushroom,
+            cardTypes.slime,
+            cardTypes.yeti,
+            cardTypes.club,
+            cardTypes.spade,
+            cardTypes.heart,
+            cardTypes.diamond,
+            cardTypes.joker,
+            cardTypes.orange_mushroom,
+            cardTypes.slime,
+            cardTypes.yeti,
+        };
 
         int i = 0;
 
-        for (int x = 0; x < 4; x++){
-            
-            for (int y = 0; y < 4; y++){
-                CardUI clone = Instantiate(prefabCardUI,canvas);
-                clone.transform.localPosition = new Vector3(x_pos + 200* x, y_pos - 250 * y);
-                 clone.cardType = cardsType[i];
+        for (int x = 0; x < max_x; x++)
+        {
+            for (int y = 0; y < max_y; y++)
+            {
+                
+                CardUI clone = Instantiate(prefabCardUI, canvas);
+                clone.transform.localPosition = new Vector3(x_pos + 200 * x, y_pos - 250 * y);
+                clone.cardType = cardsType[i];
                 switch (clone.cardType)
                 {
                     case cardTypes.diamond:
@@ -115,16 +146,34 @@ public class CardGameMain : MonoBehaviour
                         clone.frontImage.sprite = yetiSprite;
                         break;
                 }
+                listOfCardsObject.Add(clone.gameObject);
                 i++;
-
             }
         }
+    }
 
+    void DestroyAllCards()
+    {
+        foreach (var card in listOfCardsObject)
+        {
+            Destroy(card);
+        }
+    }
+
+    void Start()
+    {
+        main = this;
+        Debug.Log("am i called?");
+        StartGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (count == ((max_x * max_y) / 2))
+        {
+            DestroyAllCards();
+            StartGame();
+        }
     }
 }
--- FinalYearProject/Assets/CardUI.cs
+++ FinalYearProject/Assets/CardUI.cs
@@ -5,23 +5,40 @@ using UnityEngine.UI;
 
 public class CardUI : MonoBehaviour
 {
-    public bool matched;
-
+    public bool isAlreadyMatchedWithAnotherCard;
     public Image frontImage;
     public CardGameMain.cardTypes cardType;
     public Animator anim;
-    public void PlayAnim(string AnimName){
-        anim.Play(AnimName);
+    public void PlayAnim(string AnimName, System.Action funcCall)
+    {
+        anim.speed = 0.25f;
+        StartCoroutine(PlayAnimationAfterCurrent("FlipClose",funcCall));
+        anim.Play(AnimName, 0, 0);
 
     }
-    public void onCardPress(){
-        if (matched){
+
+    public void onCardPress()
+    {
+        if (isAlreadyMatchedWithAnotherCard)
+        {
             return;
-            
+
         }
         CardGameMain.main.onCardSelection(this);
-        
+
+    }
+    IEnumerator PlayAnimationAfterCurrent(string Animation, System.Action funcCall)
+    {
+        // Play the first animation
+        anim.Play(Animation);
+
+        // Wait for the first animation to end
+        yield return new WaitForSeconds(4/6f);
+        funcCall();
+
     }
 
-    
+
+
 }
+
